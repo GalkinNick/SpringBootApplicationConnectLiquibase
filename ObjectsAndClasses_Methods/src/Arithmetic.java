c69760bed37a7fb2d09690866b007d2dd1f667e9
Nickolay Galkin <nicolas_galkin@mail.ru>
2023-08-13 21:26:51 +0500

    homework 3 test 1 done

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,3 +1,43 @@
 public class Arithmetic {
+    private int a;
+    private int b;
+
+
+
+    public Arithmetic(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sum (){
+        int sum = a + b;
+        return sum;
+    }
+
+    public int composition(){
+        int comp = a * b;
+        return comp;
+    }
+    public int max(){
+        int max;
+        if ( a > b){
+            max = a;
+        }
+        else {
+            max = b;
+        }
+        return max;
+    }
+    public int min(){
+        int min;
+        if ( a < b){
+            min = a;
+        }
+        else {
+            min = b;
+        }
+        return min;
+    }
+
 
 }
