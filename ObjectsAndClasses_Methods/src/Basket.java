d65f4dcfb5512b327dc12bda23b6ea4fb1f8e1d1
Nickolay Galkin <nicolas_galkin@mail.ru>
2023-08-14 19:32:47 +0500

    удалил метод add с двумя параметрами, так как вообще в задании ни слова о его вызове

--- ObjectsAndClasses_Methods/src/Basket.java
+++ ObjectsAndClasses_Methods/src/Basket.java
@@ -32,7 +32,7 @@ public class Basket {
     }
 
     public void add(String name, int price) {
-        add(name, price);
+        
     }
 
     public void add(String name, int price, int count, double weight) {
