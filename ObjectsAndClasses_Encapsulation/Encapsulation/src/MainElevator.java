bd49b6fc2429adc76fabc0d0f60425ad69ea82cc
Nickolay Galkin <nicolas_galkin@mail.ru>
2023-08-19 04:22:27 +0500

    Крайняя правка задания

--- ObjectsAndClasses_Encapsulation/Encapsulation/src/MainElevator.java
+++ ObjectsAndClasses_Encapsulation/Encapsulation/src/MainElevator.java
@@ -4,11 +4,11 @@ public class MainElevator {
     public static void main(String[] args) {
         Elevator elevator = new Elevator(-3, 26);
         while (true) {
-            System.out.print("Введите номер этажа: ");
-
+            System.out.print("Введите номер этажа: ");//
             int floor = new Scanner(System.in).nextInt();
             elevator.move(floor);
             //  System.out.println(elevator.getCurrentFloor());
+
         }
     }
 }
