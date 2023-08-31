b6bb730beac9ebecf99ea1969d8dfb01416c222d
Nickolay Galkin <nicolas_galkin@mail.ru>
2023-08-31 22:30:49 +0500

    ver 1

--- Primitives/IntOrInteger/src/Container.java
+++ Primitives/IntOrInteger/src/Container.java
@@ -1,9 +1,10 @@
 public class Container {
-    private Integer count;
+    private int count;
 
     public void addCount(int value) {
         count = count + value;
     }
+
 
     public int getCount() {
         return count;
--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -1,12 +1,30 @@
+import java.net.SocketOption;
+
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
 
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+        Capital();
+        Uppercase();
 
+    }
+    // TODO: ниже напишите код для выполнения задания:
+    //  С помощью цикла и преобразования чисел в символы найдите все коды
+    //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+    private static void Capital(){
+        for (char ch = 'А'; ch <= 'Я'; ch++) {
+            char chr = ch;
+            int in = (int)chr;
+            System.out.println(ch + "(" + in + ")" );
+        }
+    }
+    private static void Uppercase() {
+        for (char ch1 = 'а'; ch1 <= 'я'; ch1++) {
+            char chr1 = ch1;
+            int in1 = (int) chr1;
+            System.out.println(ch1 + "(" + in1 + ")" );
+        }
     }
 }
