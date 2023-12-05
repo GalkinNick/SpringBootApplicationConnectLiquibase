c7ea6bbbfa56ea9570ffea9dbc443ce7f2851ba6
Nickolay Galkin <nicolas_galkin@mail.ru>
2023-12-05 21:55:07 +0500

    ver 1.0

--- AbstractClasses/practice_1/src/Company.java
+++ AbstractClasses/practice_1/src/Company.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Company implements Comparator<Employee>{
+public class Company {
 
     private String name;
     private final int MIN_INCOME = 115000;
@@ -41,12 +41,12 @@ public class Company implements Comparator<Employee>{
 
 
    public ArrayList<Employee> getTopSalaryStaff(int count){
-     //  Collections.sort(employeesCompany);
+       //  Collections.sort(employeesCompany);
        return null;
    }
 
    public ArrayList<Employee> getLowesSalaryStaff(int count){
-    /*    ArrayList<Employee> lowesSalary = new ArrayList<>();
+       ArrayList<Employee> lowesSalary = new ArrayList<>();
        employeesCompany.sort(Collections.reverseOrder());
         int i = 0;
        for ( Employee emp : employeesCompany){
@@ -56,12 +56,9 @@ public class Company implements Comparator<Employee>{
        }
 
         return lowesSalary;
-*/ return  null;
+
    }
 
 
-    @Override
-    public int compare(Employee o1, Employee o2) {
-        return Integer.compare((int) o1.getMonthSalary(), (int) o2.getMonthSalary());
-    }
+
 }
