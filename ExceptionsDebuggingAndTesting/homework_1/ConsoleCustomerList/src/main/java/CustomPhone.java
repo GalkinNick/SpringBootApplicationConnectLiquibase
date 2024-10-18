4845dadec1adea4184f9c009450e05f3b7f9743a
GalkinNick <144416222+GalkinNick@users.noreply.github.com>
2024-10-18 22:13:21 +0500

    Delete ExceptionsDebuggingAndTesting directory

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomPhone.java
+++
@@ -1,2 +0,0 @@
-public class CustomPhone extends Exception {
-}
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/MyCustom.java
+++
@@ -1,9 +0,0 @@
-public class MyCustom extends Exception{
-    public MyCustom(String message){
-        super(message);
-    }
-
-    public MyCustom(String message, Throwable cause){
-        super(message, cause);
-    }
-}
